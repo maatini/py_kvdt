dba94ca9cda0f5df7b7208a4612da5a34a07ac28
martin <martin.richardt@googlemail.com>
2014-02-08 17:16:29 +0100

    i. A.

--- kvdt_main.py
+++ kvdt_main.py
@@ -2,10 +2,11 @@
 
 __author__ = 'Martin'
 
+import re
 
 import kvdt_reader
 import kvdt_feld_stm
-
+from kvdt_satzarten import *
 
 
 def check_kvdt_felder(tokens):
@@ -27,122 +28,85 @@ def check_kvdt_felder(tokens):
     print "\n\nAufgetretene Fehler:", err_cnt
 
 
-def parse(tokens):
-    current = 0
+class Lexer:
+    def __init__(self, sequence):
+        self.sequence = sequence
+        self.len = len(sequence)
+        self.pos = 0
 
-    def match(fk, attr=None):
-        return fk == tokens[current].type and (attr is None or attr == tokens[current].attr)
+    def value(self):
+        return self.sequence[self.pos] if self.pos <self.len else None
 
-    def match_or_except(fk, attr=None):
-        if not(fk == tokens[current].type and (attr is None or attr == tokens[current].attr)):
-            raise Exception('%s/%s erwartet:%s' %(fk, '' if attr is None else attr, tokens[current]))
+    def advance(self, n = 1):
+        self.pos += n
+        print self.sequence[self.pos].type if self.pos < self.len else 'EOF'
+        return self.pos < self.len
 
-    def read_con0(current):
-        match_or_except('9103'); current += 1
-        match_or_except('9106'); current += 1
-        match_or_except('9132'); current += 1
-        while match('9132'):
-            current += 1
-
-        return current
-
-    def read_besa(current):
-        # (N)BSNR
-        match_or_except('0201')
-        while match('0201'):
-            current += 1
-            # (N)BSNR-Bezeichnung
-            match_or_except('0203')
-            current += 1
-            # LANR
-            match_or_except('0212')
-            while match('0212'):
-                current += 1
-                # Titel des Arztes
-                if match('0219'): current += 1
-                # Arztvorname
-                if match('0220'): current += 1
-                # Namenszusatz des Arztes
-                if match('0221'): current += 1
-                # Arztname oder Erläuterung
-                if match('0211'): current += 1
-
-            # Straße der (N)BSNR-Adresse
-            match_or_except('0205'); current += 1
-            # PLZ der (N)BSNR-Adresse
-            match_or_except('0215'); current += 1
-            # Ort der (N)BSNR-Adresse
-            match_or_except('0216'); current += 1
-            # Telefonnummer
-            match_or_except('0208'); current += 1
-            # Telefaxnummer
-            if match('0209'): current += 1
-            # E-Mail der (N)BSNR /Praxis
-            if match('0218'): current += 1
-        return current
-
-    def rvsa(current):
-        match_or_except('0201')
-        while match('0201'):
-            current += 1
-            match_or_except('0300')
-
-            match_or_except('0301')
-            while match('0302'):
-                current += 1
-                match_or_except('0303')
-
-            while match('0304'):
-                current += 1
-                match_or_except('0305')
+    def valid(self):
+        return self.pos < self.len
 
 
+def parse_struktur(lexer, struktur):
+    # FK, BEZEICHNER, ANZAHL, MUSSKANN, REGELN, SUBFELDER
+
+    for f in struktur:
+        v = lexer.value()
+
+        # bestimmte erwartet
+        if f[MUSSKANN] == 'm' and len(f[REGELN]) == 0 and f[FK] != v.type:
+                raise Exception('parser-fehler in %s: %s erwartet, %s gefunden' % (struktur[0][BEZEICHNER], f[FK], v.type))
+
+        n = f[ANZAHL]
+        while f[FK] == v.type and n != 0:
+            n -= 1
+            if not lexer.advance():
+                break
+
+            if len(f[SUBFELDER]) > 0:
+                parse_struktur(lexer, f[SUBFELDER])
+
+        i += 1
+        if not lexer.valid():
+            break
+
+    # alle Wert-Felder im Satz in Struktur ge-'parsed'
+    # nicht ge-'parsed' Strukturfelder müssen optional sein,
+    # damit es passt
+
+    while i < len(struktur) and struktur[i][MUSSKANN] == 'k':
+        i += 1
+    print
+    return i >= len(struktur)
 
 
+def parse(saetze):
+    # allgemeiner Paket-Aufbau
+    # con0 besa [rvsa] adt0 {010?} adt9 kadt0 {0109} kadt9 sadt0 {sad?} sadt9 con9
 
-    match_or_except('8000', 'con0')
-    current += 1
-
-    # BESA
-    if match('0201'):
-        while match('0201'):
-            current = read_besa(current+1)
-
-    # RVSA
-    if tokens[current].match('8000', 'rvsa'):
-        current = read_rvsa(current+1)
-
-    # ADT
-    if tokens[current].match('8000', 'adt0'):
-        current = read_adt0(current)
-        while tokens[current].match('8000') and tokens[current].attr in ['0101', '0102', '0103', '0104']:
-            if tokens[current].attr == '0101':
-                current = parse_0101(current)
-            elif tokens[current].attr == '0102':
-                current = parse_0102(current)
-            elif tokens[current].attr == '0103':
-                current = parse_0103(current)
-            else:
-                current = parse_0104(current)
-
-        if not tokens[current].match('8000', 'adt9'):
-            raise Exception('8000/adt9 erwartet:'+tokens[current])
-
+    # konkrerter ADT-Paket-Aufbau
+    # con0 besa rvsa adt0 {0101|0102|0103|0104} adt9 con9
+    re_adt = re.compile(r'con0 besa rvsa adt0 ((0101 )|(0102 )|(0103 )|(0104 ))+\s*adt9 con9')
+    s = ' '.join(map(lambda x: x[0].attr, saetze))
+    print s
+    if re_adt.match(s):
+        print "ADT-konformes Paket"
+        for s in saetze:
+            print s[0].attr
+            parse_struktur(Lexer(s), NAME_2_STRUKTUR[s[0].attr])
     else:
-        raise Exception('8000/adt0 erwartet:'+tokens[current])
+        print "nicht ADT-konformes Paket!"
 
 
 def parse_demo():
     reader = kvdt_reader.KVDT_Reader()
-    tokens = kvdt_reader.scan(r'H:\work\kvdt_filter\Z05123456699_30.06.2013_12.00.CON')
+    saetze = kvdt_reader.scan(r'H:\work\kvdt_filter\kvdt_data02.con')
 
-    check_kvdt_felder(tokens)
-    print len(tokens), "Felder gelesen"
+    #check_kvdt_felder(tokens)
+    print len(saetze), "Sätze gelesen"
 
-    parse(tokens)
+    parse(saetze)
 
-from satz_stm import KVDT_Stamm
 
-DIRPATH = r'H:\work\kvdt_filter\src\satzarten'
-stm = KVDT_Stamm(DIRPATH)
+
+parse_demo()
 
--- kvdt_reader.py
+++ kvdt_reader.py
@@ -9,14 +9,22 @@ class KVDT_Reader:
     """
 
     def __init__(self):
-        self.tokens_ = []
+        self.saetze = []
 
     def tokenize(self, filespec):
         f = open(filespec, "r")
+        satz = []
         for line in f.readlines():
-            self.tokens_.append(Token(line[3:7], line[7:-1]))
+            t = Token(line[3:7], line[7:-1])
+            if t.type == "8000":
+                if len(satz) > 0:
+                    self.saetze.append(satz[:])
+                    satz = []
+            satz.append(t)
+        self.saetze.append(satz[:])
+
         f.close()
-        return self.tokens_
+        return self.saetze
 
 
 def scan(filespec):
